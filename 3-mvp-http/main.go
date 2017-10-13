2addf5069a48c11237e215b812c95965fcb01158
Nick Stogner <nicholas.stogner@gmail.com>
2017-10-12 20:39:42 -0400

    Add 3-mvp-http code

--- 3-mvp-http/main.go
+++ 3-mvp-http/main.go
@@ -1,4 +1,61 @@
 package main
 
+import (
+	"bufio"
+	"log"
+	"net"
+)
+
 func main() {
+	l, err := net.Listen("tcp", ":7000")
+	if err != nil {
+		log.Fatalln("unable to listen:", err)
+	}
+	defer l.Close()
+
+	log.Print("listening")
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Println("unable to accept")
+			break
+		}
+
+		serve(conn)
+	}
+}
+
+func serve(c net.Conn) {
+	defer c.Close()
+
+	r := bufio.NewReader(c)
+
+	// Read request line
+	ln0, err := r.ReadString('\n')
+	if err != nil {
+		return
+	}
+	log.Printf("read request line: %q", ln0)
+
+	// Read headers
+	for {
+		ln, err := r.ReadString('\n')
+		if err != nil {
+			break
+		}
+
+		// An empty line with crlf marks the end of the headers
+		if ln == "\r\n" {
+			break
+		}
+
+		// Must be a header if we have reached this point
+		log.Printf("read request header: %q", ln)
+	}
+
+	// Ignore body
+
+	// Write response
+	c.Write([]byte("HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n"))
 }
