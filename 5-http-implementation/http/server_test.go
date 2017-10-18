07b71bffb87673f72381e3f5a353c6f8554bcdf2
Nick Stogner <nstogner@redventures.com>
2017-10-18 18:28:29 -0400

    Add to tests

--- 5-http-implementation/http/server_test.go
+++ 5-http-implementation/http/server_test.go
@@ -5,14 +5,19 @@ import (
 	"io/ioutil"
 	"net"
 	stdhttp "net/http"
+	"strconv"
 	"testing"
 
 	"github.com/nstogner/learning-http/5-http-implementation/http"
 )
 
 func TestServe(t *testing.T) {
-	const body = `{"abc":123}`
+	const (
+		reqBody = `{"abc":123}`
+		resBody = `{"xyz":456}`
+	)
 
+	// Start a listener on any free port.
 	l, err := net.Listen("tcp", "")
 	if err != nil {
 		t.Fatal("unable to listen:", err)
@@ -20,6 +25,7 @@ func TestServe(t *testing.T) {
 
 	var th testHandler
 	th.response.status = 201
+	th.response.body = []byte(resBody)
 	server := http.Server{
 		Handler: &th,
 	}
@@ -33,18 +39,41 @@ func TestServe(t *testing.T) {
 	if err != nil {
 		t.Fatal("unable to split host and port:", err)
 	}
-	resp, err := stdhttp.Post("http://localhost:"+port, "application/json", bytes.NewReader([]byte(body)))
+	resp, err := stdhttp.Post("http://localhost:"+port, "application/json", bytes.NewReader([]byte(reqBody)))
 	if err != nil {
 		t.Fatal("post failed:", err)
 	}
-	if resp.StatusCode != th.response.status {
+
+	// Check that the response set by the testHandler match the response that
+	// was received.
+	if th.response.status != resp.StatusCode {
 		t.Fatalf("expected status code %s, got: %s", th.response.status, resp.StatusCode)
 	}
-	if string(th.request.body) != body {
-		t.Fatalf("expected body '%s', got: '%s'", body, string(th.request.body))
+
+	// Check that expected response headers are set.
+	if len(resp.Header.Get("Date")) == 0 {
+		t.Fatal("expected header 'Date' to be set")
+	}
+	if cl, err := strconv.Atoi(resp.Header.Get("Content-Length")); err == nil {
+		if exp := len(resBody); cl != exp {
+			t.Fatalf("expected header 'Content-Length' = %v, got: %v", exp, cl)
+		}
+	} else {
+		t.Fatal("unable to parse 'Content-Length' header:", err)
+	}
+
+	// Check that the fields that were parsed by the Server match what was
+	// requested.
+	if exp := "POST"; th.request.method != exp {
+		t.Fatalf("expected method %s, got: %s", exp, th.request.method)
+	}
+	if string(th.request.body) != reqBody {
+		t.Fatalf("expected body '%s', got: '%s'", reqBody, string(th.request.body))
 	}
 }
 
+// testHandler records parsed request fields and sets predefined response
+// fields.
 type testHandler struct {
 	request struct {
 		method string
@@ -52,9 +81,11 @@ type testHandler struct {
 	}
 	response struct {
 		status int
+		body   []byte
 	}
 }
 
+// ServeHTTP satisfies the http.Handler interface.
 func (th *testHandler) ServeHTTP(res *http.Response, req *http.Request) {
 	th.request.method = req.Method
 
@@ -63,4 +94,5 @@ func (th *testHandler) ServeHTTP(res *http.Response, req *http.Request) {
 	th.request.body, _ = ioutil.ReadAll(req.Body)
 
 	res.Status = th.response.status
+	res.Write(th.response.body)
 }
